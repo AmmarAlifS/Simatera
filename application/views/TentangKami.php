ee3c336f663512e50b84d81a352fb1ee5aca41fa
Ammar Alif <ammaralifs@gmail.com>
2023-03-15 11:13:30 +0700

    Update Tentang Kami

--- application/views/TentangKami.php
+++ application/views/TentangKami.php
@@ -27,7 +27,7 @@
 
   <!-- Template Main CSS Files -->
   <link href="<?php echo base_url()?>assets/css/variables.css" rel="stylesheet">
-  <link href="<?php echo base_url()?>assets/css/main.css" rel="stylesheet">
+  <link href="<?php echo base_url()?>assets/css/tentangkami.css" rel="stylesheet">
 </head>
 
 <body>
@@ -216,18 +216,16 @@
         <div class="row g-5">
           <div class="col-lg-4">
             <h3 class="footer-heading">About ZenBlog</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ab, perspiciatis beatae autem deleniti voluptate nulla a dolores, exercitationem eveniet libero laudantium recusandae officiis qui aliquid blanditiis omnis quae. Explicabo?</p>
+            <p>Selamat datang di website kami! Kami adalah platform yang didedikasikan untuk menyediakan informasi dan layanan yang bermanfaat bagi pengunjung kami. Website kami menyajikan berbagai informasi terkini dan terpercaya tentang tempat bersejarah di kota Bandung.</p>
             <p><a href="about.html" class="footer-link-more">Learn More</a></p>
           </div>
           <div class="col-6 col-lg-2">
             <h3 class="footer-heading">Navigation</h3>
             <ul class="footer-links list-unstyled">
               <li><a href="index.html"><i class="bi bi-chevron-right"></i> Home</a></li>
-              <li><a href="index.html"><i class="bi bi-chevron-right"></i> Blog</a></li>
-              <li><a href="category.html"><i class="bi bi-chevron-right"></i> Categories</a></li>
-              <li><a href="single-post.html"><i class="bi bi-chevron-right"></i> Single Post</a></li>
-              <li><a href="about.html"><i class="bi bi-chevron-right"></i> About us</a></li>
-              <li><a href="contact.html"><i class="bi bi-chevron-right"></i> Contact</a></li>
+              <li><a href="index.html"><i class="bi bi-chevron-right"></i> Artikel</a></li>
+              <li><a href="about.html"><i class="bi bi-chevron-right"></i> Tentang Kami</a></li>
+              <li><a href="contact.html"><i class="bi bi-chevron-right"></i> Kontak</a></li>
             </ul>
           </div>
           <div class="col-6 col-lg-2">
@@ -251,7 +249,7 @@
             <ul class="footer-links footer-blog-entry list-unstyled">
               <li>
                 <a href="single-post.html" class="d-flex align-items-center">
-                  <img src="assets/img/post-sq-1.jpg" alt="" class="img-fluid me-3">
+                  <img src="<?php echo base_url()?>assets/img/post-sq-1.jpg" alt="" class="img-fluid me-3">
                   <div>
                     <div class="post-meta d-block"><span class="date">Culture</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
                     <span>5 Great Startup Tips for Female Founders</span>
@@ -261,7 +259,7 @@
 
               <li>
                 <a href="single-post.html" class="d-flex align-items-center">
-                  <img src="assets/img/post-sq-2.jpg" alt="" class="img-fluid me-3">
+                  <img src="<?php echo base_url()?>assets/img/post-sq-2.jpg" alt="" class="img-fluid me-3">
                   <div>
                     <div class="post-meta d-block"><span class="date">Culture</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
                     <span>What is the son of Football Coach John Gruden, Deuce Gruden doing Now?</span>
@@ -271,7 +269,7 @@
 
               <li>
                 <a href="single-post.html" class="d-flex align-items-center">
-                  <img src="assets/img/post-sq-3.jpg" alt="" class="img-fluid me-3">
+                  <img src="<?php echo base_url()?>assets/img/post-sq-3.jpg" alt="" class="img-fluid me-3">
                   <div>
                     <div class="post-meta d-block"><span class="date">Culture</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
                     <span>Life Insurance And Pregnancy: A Working Mom’s Guide</span>
@@ -281,7 +279,7 @@
 
               <li>
                 <a href="single-post.html" class="d-flex align-items-center">
-                  <img src="assets/img/post-sq-4.jpg" alt="" class="img-fluid me-3">
+                  <img src="<?php echo base_url()?>assets/img/post-sq-4.jpg" alt="" class="img-fluid me-3">
                   <div>
                     <div class="post-meta d-block"><span class="date">Culture</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
                     <span>How to Avoid Distraction and Stay Focused During Video Calls?</span>
