eb50e74d78c036fc4e7f1bba5022c8e9799b1593
FrostDusk <gyfasya13@gmail.com>
2023-03-31 10:46:24 +0700

    update database

---
+++ assets/database/user_masukan.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 31, 2023 at 05:43 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `simatera`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_masukan`
+--
+
+CREATE TABLE `user_masukan` (
+  `id_pesan` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `subjek` varchar(50) NOT NULL,
+  `pesan` varchar(200) NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_masukan`
+--
+
+INSERT INTO `user_masukan` (`id_pesan`, `nama`, `email`, `subjek`, `pesan`, `timestamp`) VALUES
+(1, 'a', 'a@a', '', 'a', '2023-03-27 13:55:05'),
+(2, 'a', 'a@a', 'a', 'a', '2023-03-27 13:55:05'),
+(3, 's', 's@s', 's', 's', '2023-03-27 13:55:05'),
+(4, 's', 's@ss', 's', 's', '2023-03-27 13:55:05'),
+(5, 'sdsa', 'dsa@dsa', 'dsa', 'das', '2023-03-27 13:55:05'),
+(6, 'das', 'dsa@ds', 'dsa', 'dsa', '2023-03-27 13:55:05'),
+(7, 'dsa', 'dsa@dsa', 'dsa', 'dsa', '2023-03-27 13:55:05'),
+(8, 'dsa', 'dsa@dsa', 'das', 'das', '2023-03-27 13:55:05'),
+(9, 'das', 'da@da', 'da', 'dsa', '2023-03-27 13:55:05'),
+(10, 'dsa', 'das@dsa', 'dsa', 'dsa', '2023-03-27 13:55:05'),
+(11, 'dsa', 'das@dsa', 'dsa', 'dsa', '2023-03-27 13:55:05'),
+(12, 'dsa', 'dsa@dsa', 'dsa', 'das', '2023-03-27 13:55:05'),
+(13, 'dsa', 'dsadsad@dsa', 'dsadsa', 'dsad', '2023-03-27 13:55:05'),
+(14, 'dsadsa', 'dsaddsa@dada', 'das', 'das', '2023-03-27 13:55:05'),
+(15, 'dsa', 'dsa@das', 'dsa', 'dsa', '2023-03-27 13:55:05'),
+(16, 'cxz', 'cxz2s@dsa', 'das', 'dsa', '2023-03-27 13:55:05'),
+(17, 'sda', 'dsadsas@dsad', 'saddsa', 'dsadas', '2023-03-27 13:55:05'),
+(18, 'dsa', 'das@dsa', 'dsa', 'das', '2023-03-27 13:55:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_masukan`
+--
+ALTER TABLE `user_masukan`
+  ADD PRIMARY KEY (`id_pesan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_masukan`
+--
+ALTER TABLE `user_masukan`
+  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
