663a0f1def395e9650b95f9296dd7c8a6dc16989
SilverK <Arrijalm12@gmail.com>
2023-04-05 14:08:19 +0700

    forgot password

--- assets/database/simatera.sql
+++ assets/database/simatera.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 05 Apr 2023 pada 06.35
--- Versi server: 10.1.36-MariaDB
--- Versi PHP: 7.2.10
+-- Generation Time: Mar 31, 2023 at 05:43 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `artikel_simatera`
+-- Table structure for table `artikel_simatera`
 --
 
 CREATE TABLE `artikel_simatera` (
@@ -34,132 +33,109 @@ CREATE TABLE `artikel_simatera` (
   `artikel` varchar(10000) NOT NULL,
   `Foto` varchar(255) NOT NULL,
   `Video` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `artikel_simatera`
+-- Dumping data for table `artikel_simatera`
 --
 
 INSERT INTO `artikel_simatera` (`judul_id`, `judul`, `artikel`, `Foto`, `Video`) VALUES
 (0, 'Sejarah Balai Kota Bandung', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero temporibus repudiandae, inventore pariatur numquam cumque possimus', 'BalaiKotaBandung.jpg', 'BalaiKotaBandung.mp4'),
-(1, 'Sejarah Balai Kota Bandung', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero temporibus repudiandae, inventore pariatur numquam cumque possimus', 'BalaiKotaBandung.jpg', 'BalaiKotaBandung.mp4'),
-(0, 'a', '<p>a</p>', '', ''),
-(0, 'a', '<p>a</p>', '', ''),
-(0, 's', '<p>s</p>', '', ''),
-(0, 't', '<p>t</p>', '', ''),
-(0, 'z', '<p>z</p>', '', ''),
-(0, 'w', '<p>w</p>', '', '');
+(1, 'Sejarah Balai Kota Bandung', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero temporibus repudiandae, inventore pariatur numquam cumque possimus', 'BalaiKotaBandung.jpg', 'BalaiKotaBandung.mp4');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `login_admin`
+-- Table structure for table `login_admin`
 --
 
 CREATE TABLE `login_admin` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `login_admin`
+-- Dumping data for table `login_admin`
 --
 
 INSERT INTO `login_admin` (`id`, `username`, `email`, `password`) VALUES
-(1, 's', 's@s', 's'),
-(2, 'q', 'q@a', 'a'),
-(3, 'sada', 'dadas@dsaf', 'adasa'),
-(4, 'R', 'Rekid127@gmail.com', 'Kanekiken'),
-(7, 'dsasa', 'rijalfauziiskandar93.rfi@gmail.com', 'jsafsasfa'),
-(8, 'FSAFSA', 'rijalfauziiskandar4@gmail.com', 'fsabsajasj');
+(1, 'a', 'a@a', 'a'),
+(2, 'b', 'b@b', 'b'),
+(3, 'c', 'c@c', 'c'),
+(4, 'aa', 'aa@a', 'a');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `reset_token` varchar(255) DEFAULT NULL,
-  `reset_timestamp` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `user_masukan`
+-- Table structure for table `user_masukan`
 --
 
 CREATE TABLE `user_masukan` (
+  `id_pesan` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `subjek` varchar(50) NOT NULL,
-  `pesan` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `pesan` varchar(200) NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `user_masukan`
+-- Dumping data for table `user_masukan`
 --
 
-INSERT INTO `user_masukan` (`nama`, `email`, `subjek`, `pesan`) VALUES
-('a', 'a@a', '', 'a'),
-('a', 'a@a', 'a', 'a'),
-('s', 's@s', 's', 's'),
-('s', 's@ss', 's', 's'),
-('sdsa', 'dsa@dsa', 'dsa', 'das'),
-('das', 'dsa@ds', 'dsa', 'dsa'),
-('dsa', 'dsa@dsa', 'dsa', 'dsa'),
-('dsa', 'dsa@dsa', 'das', 'das'),
-('das', 'da@da', 'da', 'dsa'),
-('dsa', 'das@dsa', 'dsa', 'dsa'),
-('dsa', 'das@dsa', 'dsa', 'dsa'),
-('dsa', 'dsa@dsa', 'dsa', 'das'),
-('dsa', 'dsadsad@dsa', 'dsadsa', 'dsad'),
-('dsadsa', 'dsaddsa@dada', 'das', 'das'),
-('dsa', 'dsa@das', 'dsa', 'dsa'),
-('cxz', 'cxz2s@dsa', 'das', 'dsa'),
-('sda', 'dsadsas@dsad', 'saddsa', 'dsadas'),
-('dsa', 'das@dsa', 'dsa', 'das');
+INSERT INTO `user_masukan` (`id_pesan`, `nama`, `email`, `subjek`, `pesan`, `timestamp`) VALUES
+(1, 'a', 'a@a', '', 'a', '2023-03-27 13:55:05'),
+(2, 'a', 'a@a', 'a', 'a', '2023-03-27 13:55:05'),
+(3, 's', 's@s', 's', 's', '2023-03-27 13:55:05'),
+(4, 's', 's@ss', 's', 's', '2023-03-27 13:55:05'),
+(5, 'sdsa', 'dsa@dsa', 'dsa', 'das', '2023-03-27 13:55:05'),
+(6, 'das', 'dsa@ds', 'dsa', 'dsa', '2023-03-27 13:55:05'),
+(7, 'dsa', 'dsa@dsa', 'dsa', 'dsa', '2023-03-27 13:55:05'),
+(8, 'dsa', 'dsa@dsa', 'das', 'das', '2023-03-27 13:55:05'),
+(9, 'das', 'da@da', 'da', 'dsa', '2023-03-27 13:55:05'),
+(10, 'dsa', 'das@dsa', 'dsa', 'dsa', '2023-03-27 13:55:05'),
+(11, 'dsa', 'das@dsa', 'dsa', 'dsa', '2023-03-27 13:55:05'),
+(12, 'dsa', 'dsa@dsa', 'dsa', 'das', '2023-03-27 13:55:05'),
+(13, 'dsa', 'dsadsad@dsa', 'dsadsa', 'dsad', '2023-03-27 13:55:05'),
+(14, 'dsadsa', 'dsaddsa@dada', 'das', 'das', '2023-03-27 13:55:05'),
+(15, 'dsa', 'dsa@das', 'dsa', 'dsa', '2023-03-27 13:55:05'),
+(16, 'cxz', 'cxz2s@dsa', 'das', 'dsa', '2023-03-27 13:55:05'),
+(17, 'sda', 'dsadsas@dsad', 'saddsa', 'dsadas', '2023-03-27 13:55:05'),
+(18, 'dsa', 'das@dsa', 'dsa', 'das', '2023-03-27 13:55:05');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `login_admin`
+-- Indexes for table `login_admin`
 --
 ALTER TABLE `login_admin`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `user_masukan`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE `user_masukan`
+  ADD PRIMARY KEY (`id_pesan`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `login_admin`
+-- AUTO_INCREMENT for table `login_admin`
 --
 ALTER TABLE `login_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `user_masukan`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `user_masukan`
+  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
