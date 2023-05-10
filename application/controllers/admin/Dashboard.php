67a7709ea4e41f1874880c892e51f59520a19e56
SilverK <Arrijalm12@gmail.com>
2023-05-10 11:20:11 +0700

    dashboard

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -11,14 +11,21 @@ class Dashboard extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('admin/themes/header.php');
-		$this->load->view('admin/themes/side_nav.php');
-		$this->load->view('admin/main/dashboard.php');
-		$this->load->view('admin/themes/footer.php');
+		
 
-		$total_users = $this->db->count_all_results('login_admin');
-		$data['total_users'] = $total_users;
-		$this->load->view('admin/main/Dashboard/index', $data);
+		// Get the total number of users
+	    $total_users = $this->db->count_all_results('login_admin');
+	    $total_posts = $this->db->count_all_results('artikel_simatera');
+
+	    // Pass the total number of users to the view
+	    $data['total_users'] = $total_users;
+	    $data['total_posts'] = $total_posts;
+
+	    // Load the view with the data
+	    $this->load->view('admin/themes/header.php');
+		$this->load->view('admin/themes/side_nav.php');
+		$this->load->view('admin/main/dashboard.php', $data);
+		$this->load->view('admin/themes/footer.php');
 	}
 }
 ?>
