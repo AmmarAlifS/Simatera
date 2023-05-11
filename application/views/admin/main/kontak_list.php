c59cbde14c219e1fbb678df027ef5f056c2da3a9
Ammar Alif <ammaralifs@gmail.com>
2023-05-11 14:33:56 +0700

    HEHE

--- application/views/admin/main/kontak_list.php
+++ application/views/admin/main/kontak_list.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Admin | Dashboard</title>
+	<title>Admin | List Kontak</title>
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
 	<link rel="stylesheet" href="<?= base_url('assets/admin/bower_components/bootstrap/dist/css/bootstrap.min.css') ?>">
@@ -88,7 +88,6 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-</main>
 </body>
 	<script src="<?= base_url('assets/admin/bower_components/jquery/dist/jquery.min.js') ?>"></script>
 
--- application/views/admin/main/list_artikel.php
+++ application/views/admin/main/list_artikel.php
@@ -2,10 +2,10 @@
 <html lang="en">
 
 <head>
-<meta charset="utf-8">
+  <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Admin | Dashboard</title>
-	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	<title>Admin | List Artikel</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
 	<link rel="stylesheet" href="<?= base_url('assets/admin/bower_components/bootstrap/dist/css/bootstrap.min.css') ?>">
 	
@@ -40,8 +40,8 @@
       </ol>
     </section>
 
-    <!-- Main content -->
-    <section class="content">
+   <!-- Main content -->
+   <section class="content">
       <div class="row">
         <div class="col-xs-12">
           <div class="box">
@@ -53,12 +53,12 @@
               <table id="example1" class="table table-bordered table-striped">
                 <thead>
                   <tr>
-                  	<th>ID</th>
+				            <th>ID</th>
                     <th>Judul</th>
                     <th>Artikel</th>
                     <th>Tanggal</th>
-					<th>Vidio</th>
-					<th>Foto</th>
+					          <th>Vidio</th>
+					          <th>Foto</th>
                     <th>Action</th>
                   </tr>
                 </thead>
@@ -68,18 +68,16 @@
                     foreach($art as $u) {
                   ?>
                   <tr>
-                  	<td><?php echo $u->id_artikel ?></td>
+				            <td><?php echo $u->id_artikel ?></td>
                     <td><?php echo $u->judul ?></td>
                     <td><?php echo $u->artikel ?></td>
                     <td><?php echo $u->tanggal ?></td>
                     <td><?php echo $u->Video ?></td>
-					<td><?php echo $u->Foto ?></td>
+                    <td><img width="100" height="100" src="<?= base_url('assets/img/')?><?php echo $u->Foto ?>"></td>
 										<td><a href="" class="btn btn-primary">Update</a></td>
 	      						<td><a href="<?= base_url('admin/Artikel/delete/'.$u->id_artikel) ?>" class="btn btn-danger">Delete</a></td>
-	      						</td>
                   </tr>
                   <?php } ?>
-          
                 </tbody>
               </table>
             </div>
@@ -94,69 +92,24 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-
-
-
-
-	<!-- <div class="content">
-			<table class="table">
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th style="width: 15%;" class="text-center">Status</th>
-						<th style="width: 25%;" class="text-center">Action</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>
-							<div>Hello World!</div>
-							<div class="text-gray"><small>12 Jan 2021<small></div>
-						</td>
-						<td class="text-center text-gray">Draft</td>
-						<td>
-							<div class="action">
-								<a href="#" class="button button-small" role="button">View</a>
-								<a href="#" class="button button-small" role="button">Edit</a>
-								<a href="#" class="button button-small button-danger" role="button">Delete</a>
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<div>Hello World!</div>
-							<div class="text-gray"><small>12 Jan 2021<small></div>
-						</td>
-						<td class="text-center text-green">Published</td>
-						<td>
-							<div class="action">
-								<a href="#" class="button button-small" role="button">View</a>
-								<a href="#" class="button button-small" role="button">Edit</a>
-								<a href="#" class="button button-small button-danger" role="button">Delete</a>
-							</div>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-
-		</div> -->
+</main>
 </body>
 <script src="<?= base_url('assets/admin/bower_components/jquery/dist/jquery.min.js') ?>"></script>
 
-	<script src="<?= base_url('assets/admin/bower_components/jquery-ui/jquery-ui.min.js') ?>"></script>
+<script src="<?= base_url('assets/admin/bower_components/jquery-ui/jquery-ui.min.js') ?>"></script>
 
-	<script src="<?= base_url('assets/admin/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+<script src="<?= base_url('assets/admin/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
 
-	<script src="<?= base_url('assets/admin/bower_components/datatables.net/js/jquery.dataTables.min.js') ?>"></script>
-	<script src="<?= base_url('assets/admin/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') ?>"></script>
+<script src="<?= base_url('assets/admin/bower_components/datatables.net/js/jquery.dataTables.min.js') ?>"></script>
+<script src="<?= base_url('assets/admin/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') ?>"></script>
 
-	<script src="<?= base_url('assets/admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') ?>"></script>
+<script src="<?= base_url('assets/admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') ?>"></script>
 
-	<script src="<?= base_url('assets/admin/bower_components/jquery-slimscroll/jquery.slimscroll.min.js') ?>"></script>
+<script src="<?= base_url('assets/admin/bower_components/jquery-slimscroll/jquery.slimscroll.min.js') ?>"></script>
 
-	<script src="<?= base_url('assets/admin/bower_components/fastclick/lib/fastclick.js') ?>"></script>
+<script src="<?= base_url('assets/admin/bower_components/fastclick/lib/fastclick.js') ?>"></script>
 
-	<script src="<?= base_url('assets/admin/dist/js/adminlte.min.js') ?>"></script>
+<script src="<?= base_url('assets/admin/dist/js/adminlte.min.js') ?>"></script>
 	<script>
 		$(function () {
 			$('#example1').DataTable()
